d1cfb89b07abc9d839c94b950922c3b514a118c2
TanyaShved <tanya.shved.89@gmail.com>
2020-11-12 22:02:59 +0200

    добавляет async/await

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,8 +1,3 @@
-// import { error } from '@pnotify/core';
-// import '@pnotify/core/dist/PNotify.css';
-// import '@pnotify/core/dist/BrightTheme.css';
-
-
 const API_KEY = '19013398-a980467a71ce13bd0d53bc132';
 const BASE_URL = 'https://pixabay.com/api/';
 
@@ -11,23 +6,15 @@ export default class NewsApiService {
     this.searchQuery = "";
     this.page = 1;
   }
-    
-fetchImages() {
-  return fetch(`${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`)
-    .then(response => response.json())
-    .then(images => {
 
-      // if (images.length === 0) {
-      //   console.log('Show messege');
-      //   return;
-      // }
-
+async fetchImages() {
+  const response = await fetch(`${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`);
+  const images = await response.json();   
       this.incrementPage();
       return images.hits;
-    })
-  }
+    }
 
-  incrementPage() {
+ incrementPage() {
     this.page += 1;
   }
 
@@ -44,9 +31,40 @@ fetchImages() {
   }
 }
 
-// function noResult() {
-//     error({
-//         text: "No Result!",
-//         delay: 3000,
-//     });
+// Промисы
+
+// const API_KEY = '19013398-a980467a71ce13bd0d53bc132';
+// const BASE_URL = 'https://pixabay.com/api/';
+
+// export default class NewsApiService {
+//   constructor() {
+//     this.searchQuery = "";
+//     this.page = 1;
+//   }
+    
+// fetchImages() {
+//   return fetch(`${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`)
+//     .then(response => response.json())
+//     .then(images => {
+
+//       this.incrementPage();
+//       return images.hits;
+//     })
+//   }
+
+//   incrementPage() {
+//     this.page += 1;
+//   }
+
+//   resetPage() {
+//     this.page = 1;
+//   }
+
+//   get query() {
+//     return this.searchQuery;
+//   }
+
+//   set query(newQuery) {
+//     this.searchQuery = newQuery;
+//   }
 // }
--- src/js/imagesSearch.js
+++ src/js/imagesSearch.js
@@ -29,38 +29,45 @@ function onSearch(e) {
   e.preventDefault();
 
   newsApiService.query = e.currentTarget.elements.query.value;
-
-  if (newsApiService.query === '') {
-    clearImagesContainer();
-    loadMoreBtn.hide();
-    return enterSomthing();
-  }
-  else if (refs.imagesContainer.children.length === 0) {
-    
-    console.dir(refs.imagesContainer)
-
-        noResult();
+  try {
+    if (newsApiService.query === '') {
+      clearImagesContainer();
       loadMoreBtn.hide();
-  };
+      return enterSomthing();
+    }
 
-    
-  loadMoreBtn.show();
-  newsApiService.resetPage();
-  clearImagesContainer();
-  fetchImages();
-  // searchError();
+    loadMoreBtn.show();
+    newsApiService.resetPage();
+    clearImagesContainer();
+    fetchImages();
+  } catch (error) {
+    console.log("Ошибка!")
+  }
 }
 
-function onLoadMore() {
-  newsApiService.fetchImages().then(appendImagesMarkup).then(scrollingPage);
+async function onLoadMore() {
+  try {
+    const response = await newsApiService.fetchImages();
+  const appendImages = appendImagesMarkup(response);
+  const scrolling = scrollingPage(appendImages);
+    return scrolling;
+  }catch (error) {
+    console.log("Ошибка!")
+  }
 }
 
-function fetchImages() {
-  loadMoreBtn.disable();
-
-  newsApiService.fetchImages().then(appendImagesMarkup);
-
-  loadMoreBtn.enable();
+async function fetchImages() {
+  try {
+    loadMoreBtn.disable();
+  const response = await newsApiService.fetchImages();
+  const appendImages = appendImagesMarkup(response);
+    const mistake = searchError(appendImages);
+    loadMoreBtn.enable();
+    return mistake;
+  } catch (error) {
+    console.log("Ошибка!")
+  }
+  
 }
 
 function appendImagesMarkup(images) {
@@ -89,9 +96,105 @@ function enterSomthing() {
 function noResult() {
     error({
         text: "No Result!",
-        delay: 3000,
+        delay: 1000,
     });
 }
+
+function searchError() {
+    if (refs.imagesContainer.children.length === 0) {
+        noResult();
+      loadMoreBtn.hide();
+    }
+}
+    
+
+// Промисы
+
+// import NewsApiService from './apiService.js';
+// import imageEl from '../templates/photo-cards.hbs';
+// import LoadMoreBtn from './load-more-btn';
+
+// import { alert, error } from '@pnotify/core';
+// import '@pnotify/core/dist/PNotify.css';
+// import '@pnotify/core/dist/BrightTheme.css';
+
+// import openModal from './modal';
+
+// const refs = {
+//     searchForm: document.querySelector('.search-form'),
+//     imagesContainer: document.querySelector('.gallery'),
+//     loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+// };
+
+// const loadMoreBtn = new LoadMoreBtn({
+//   selector: '[data-action="load-more"]',
+//   hidden: true,
+// });
+
+// const newsApiService = new NewsApiService();
+
+// refs.searchForm.addEventListener('submit',onSearch);  
+// refs.loadMoreBtn.addEventListener('click', onLoadMore);
+// refs.imagesContainer.addEventListener('click', openModal);
+
+// function onSearch(e) {
+//   e.preventDefault();
+
+//   newsApiService.query = e.currentTarget.elements.query.value;
+
+//   if (newsApiService.query === '') {
+//     clearImagesContainer();
+//     loadMoreBtn.hide();
+//     return enterSomthing();
+//   }
+
+//   loadMoreBtn.show();
+//   newsApiService.resetPage();
+//   clearImagesContainer();
+//   fetchImages();
+// }
+
+// function onLoadMore() {
+//   newsApiService.fetchImages().then(appendImagesMarkup).then(scrollingPage);
+// }
+
+// function fetchImages() {
+//   loadMoreBtn.disable();
+
+//   newsApiService.fetchImages().then(appendImagesMarkup).then(searchError);
+
+//   loadMoreBtn.enable();
+// }
+
+// function appendImagesMarkup(images) {
+//   refs.imagesContainer.insertAdjacentHTML('beforeend', imageEl(images));
+// }
+
+// function clearImagesContainer() {
+//   refs.imagesContainer.innerHTML = '';
+// }
+
+// function scrollingPage() {
+//     window.scrollTo({
+//         top: refs.imagesContainer.offsetHeight,
+//         left: 0,
+//         behavior: 'smooth',
+//     });
+// }
+
+// function enterSomthing() {
+//     alert({
+//         text: "Enter something normal!'",
+//         delay: 3000,
+//     });
+// }
+
+// function noResult() {
+//     error({
+//         text: "No Result!",
+//         delay: 1000,
+//     });
+// }
 
 // function searchError() {
 //     if (refs.imagesContainer.children.length === 0) {
